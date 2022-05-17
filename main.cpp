f2e278c9c2fb5372bee6d9eb4edf99cd3ed0763c
nicosicca <100024661+nicosicca@users.noreply.github.com>
2022-05-17 18:13:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <math.h>
+
 using namespace std;
 
-int main() {
-   return 0;
+double f(double x){
+    double t;
+    t=pow(x,2)*cos(x)+1;
+    return t;
+}
+
+
+int main()
+{
+    double a,b,x,err;
+
+    cout<<f(10)<<endl;
+    do{
+        cout<<"inserire estremi"<<endl;
+        cin>>a;
+        cin>>b;
+    }while(f(a)*f(b)>=0);
+
+
+
+    do{
+        x=(a+b)/2;
+        if(f(x)==0.0)
+            cout<<x<<endl;
+        else
+        {
+            if(f(x)*f(b)<0)
+                a=x;
+            else
+                b=x;
+            err=abs(b-a)/2;
+        }
+    }while(err>=1e-6);
+
+    cout<<x<<endl;
+
+    return 0;
 }
